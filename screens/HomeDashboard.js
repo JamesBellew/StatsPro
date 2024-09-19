abda2a31cb8e8f530fe3f0e1a12b108db73f7c4a
JamesBellew <11jamesbellew@gmail.com>
2024-09-19 23:40:40 +0100

    we hawt

--- screens/HomeDashboard.js
+++ screens/HomeDashboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef, useCallback } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { LinearGradient } from "expo-linear-gradient";
+
 import {
   SafeAreaView,
   View,
@@ -29,6 +30,7 @@ import {
   faEllipsis,
   faFloppyDisk,
   faCircleCheck,
+  faCheck,
 } from "@fortawesome/free-solid-svg-icons";
 import {
   useNavigation,
@@ -49,6 +51,8 @@ export default function App({ route }) {
   const [gameIndexClicked, setGameIndexClicked] = useState(null);
   const [showGeneralAlertComp, setShowGeneralAlertComp] = useState(false);
   const [generalAlertMsg, setGeneralAlertMsg] = useState(":/");
+  const [editGameName, setEditGameName] = useState("");
+  const [showEditGameSection, setShowEditGameSection] = useState(false);
   //the below two use states are used for checking to see if there are sufficiant in progress/completted games for rendering the category text
   let inProgressRendered = false;
   let completedRendered = false;
@@ -60,7 +64,34 @@ export default function App({ route }) {
   const handleStartGame = () => {
     console.log("clicked");
   };
+  const saveSavedGameEditChanges = () => {
+    console.log("clicked the save game changes function");
 
+    const updatedSavedGame = {};
+
+    // Update the game timer to 3600
+    //  const updatedGame = { ...game, timer: 3600, gameFinihsed: true };
+
+    //  // Create a new array with the updated game
+    //  const updatedGamesArray = savedGames.map((g) =>
+    //    g.id === updatedGame.id ? updatedGame : g
+    //  );
+    //  // Update the state and AsyncStorage
+    //  setSavedGames(updatedGamesArray);
+    //  AsyncStorage.setItem("@game_data", JSON.stringify(updatedGamesArray))
+    //    .then(() => {
+    //      console.log("Game data updated in AsyncStorage");
+    //      // here i will pass the message for the notification
+    //      setShowGeneralAlertComp(true);
+    //      setGeneralAlertMsg("Game completed");
+    //      setTimeout(() => {
+    //        setShowGeneralAlertComp(false);
+    //      }, 2000);
+    //    })
+    //    .catch((error) => {
+    //      console.error("Error updating game data in AsyncStorage", error);
+    //    });
+  };
   const GeneralAlertComp = () => {
     return (
       <View className="bg-zinc-800 absolute z-50 w-full px-5 mx-auto text-center py-5 rounded-b-3xl">
@@ -262,7 +293,57 @@ export default function App({ route }) {
         console.error("Error updating game data in AsyncStorage", error);
       });
   };
+  console.log("beeeloooop[");
+  console.log(savedGames);
 
+  const editGameHandler = (game) => {
+    if (game.gameName === editGameName) {
+      console.log("no beuno, names are the same");
+    } else {
+      const newGameDetails = { ...game, gameName: editGameName };
+      const updatedGamesArray = savedGames.map((g) =>
+        g.id === game.id ? newGameDetails : g
+      );
+      setSavedGames(updatedGamesArray);
+      AsyncStorage.setItem("@game_data", JSON.stringify(updatedGamesArray))
+        .then(() => {
+          console.log("Game data updated in AsyncStorage");
+          // here i will pass the message for the notification
+          setShowGeneralAlertComp(true);
+          setGeneralAlertMsg("Game Updated");
+          setTimeout(() => {
+            setShowGeneralAlertComp(false);
+          }, 2000);
+          setEditGameName("");
+          setShowEditGameSection(false);
+        })
+        .catch((error) => {
+          console.error("Error updating game data in AsyncStorage", error);
+        });
+    }
+
+    //  const updatedGame = { ...game, timer: 3600, gameFinihsed: true };
+
+    //  // Create a new array with the updated game
+    //  const updatedGamesArray = savedGames.map((g) =>
+    //    g.id === updatedGame.id ? updatedGame : g
+    //  );
+    //  // Update the state and AsyncStorage
+    //  setSavedGames(updatedGamesArray);
+    //  AsyncStorage.setItem("@game_data", JSON.stringify(updatedGamesArray))
+    //    .then(() => {
+    //      console.log("Game data updated in AsyncStorage");
+    //      // here i will pass the message for the notification
+    //      setShowGeneralAlertComp(true);
+    //      setGeneralAlertMsg("Game completed");
+    //      setTimeout(() => {
+    //        setShowGeneralAlertComp(false);
+    //      }, 2000);
+    //    })
+    //    .catch((error) => {
+    //      console.error("Error updating game data in AsyncStorage", error);
+    //    });
+  };
   //!Main JSX return section
   return (
     <SafeAreaView className="flex-1 bg-[#12131A]">
@@ -536,7 +617,9 @@ export default function App({ route }) {
                     <TouchableOpacity
                       onPress={() => {
                         setGameIndexClicked(index);
-
+                        //also clear the edit names, this is temporary for the moment, it is a wuick way of getting rid of editing the wrong game when user clicks from one game to the next
+                        setEditGameName("");
+                        setShowEditGameSection(false);
                         // navigation.navigate("InGame", { gameData: game });
                       }}
                       onLongPress={() => setLongPressedGame(game.gameName)}
@@ -637,33 +720,49 @@ export default function App({ route }) {
 
                             {/* only want this to render is the game on not yet finsihed */}
                           </View>
-                          <View className=" mt-3 w-full h-auto">
-                            <Text className="text-gray-200 px-3 font-semibold text-md ">
-                              Details of Game
-                            </Text>
-                            <Text className="text-gray-400 mt-2 mb-1 px-3 font-normal text-sm ">
-                              Name
-                            </Text>
-                            <View className="flex-row">
-                              <TextInput
-                                className=" w-1/2  bg-gray-700 text-white px-2 py-1 ml-2 mr-1 rounded-md"
-                                placeholder="Opponent"
-                                placeholderTextColor="#ccc"
-                                // value={lineoutOverallName}
-                                // onChangeText={setLineoutOverallName}
-                              />
-                              <TouchableOpacity className="w-auto bg-red-600 mr-1 h-8 justify-center px-2 rounded-md">
-                                <Text>Home</Text>
-                              </TouchableOpacity>
-                              <TouchableOpacity className="bg-blue-600 text-center py-2 px-3 rounded-md justify-center">
-                                <FontAwesomeIcon
-                                  icon={faCircleCheck}
-                                  size={18}
-                                  color="#000"
+                          {showEditGameSection && (
+                            <View className=" mt-3 w-full h-auto">
+                              <Text className="text-gray-200 px-3 font-semibold text-md ">
+                                Details of Game
+                              </Text>
+                              <Text className="text-gray-400 mt-2 mb-1 px-3 font-normal text-sm ">
+                                Name
+                              </Text>
+                              <View className="flex-row">
+                                <TextInput
+                                  className=" w-2/5  bg-gray-700 text-white px-2 py-1 ml-2 mr-1 rounded-md"
+                                  placeholder="Opponent"
+                                  placeholderTextColor="#ccc"
+                                  value={editGameName}
+                                  onChangeText={setEditGameName}
                                 />
-                              </TouchableOpacity>
+                                <View className="w-32  flex-row space-x-1 my-auto mr-1 h-auto justify-center px-2 rounded-md">
+                                  <TouchableOpacity className="w-1/2 border-b-2 border-b-blue-600 bg-gray-600 px-2 rounded-md h-8 justify-center">
+                                    <Text className="text-center text-xs text-gray-200">
+                                      Home
+                                    </Text>
+                                  </TouchableOpacity>
+                                  <TouchableOpacity className="w-1/2 bg-gray-600 px-2 rounded-md h-8 justify-center">
+                                    <Text className="text-center text-xs text-gray-200">
+                                      Away
+                                    </Text>
+                                  </TouchableOpacity>
+                                </View>
+                                <TouchableOpacity
+                                  onPress={() => {
+                                    editGameHandler(game);
+                                  }}
+                                  className="bg-blue-600 text-center py-2 px-3 rounded-md justify-center"
+                                >
+                                  <FontAwesomeIcon
+                                    icon={faCheck}
+                                    size={18}
+                                    color="#000"
+                                  />
+                                </TouchableOpacity>
+                              </View>
                             </View>
-                          </View>
+                          )}
                           {/* <View className="flex-row w-full  px-2 mt-2 items-center mx-auto just">
                             <View className="w-2/4 h-12    flex-row items-center ">
                               <TouchableOpacity className="bg-[#0b63fb] bg-zinc-800  mr-1 rounded-full px-4 py-2 h-10 w-10">
@@ -730,7 +829,10 @@ export default function App({ route }) {
                               </Text>
                             </TouchableOpacity>
                             <TouchableOpacity
-                              onPress={() => deleteGame(game.gameName)}
+                              onPress={() => {
+                                setShowEditGameSection(true);
+                                setLongPressedGame(false);
+                              }}
                               className="bg-blue-500 w-auto my-auto mx-3 justify-center items-center px-2 rounded-md h-auto py-2 px-4"
                             >
                               <Text className="text-white px-2 text-center">
